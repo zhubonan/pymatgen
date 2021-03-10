2866048a9ddf61febd7e47f9fb0435337a65cfd7
First Coder <shyuep@gmail.com>
2021-03-10 08:29:16 -0800

    Add deprecation message to pymatgen/__init__.py

--- pymatgen/__init__.py
+++ pymatgen/__init__.py
@@ -13,6 +13,8 @@ __maintainer__ = "Shyue Ping Ong"
 __maintainer_email__ = "shyuep@gmail.com"
 __version__ = "2021.3.9"
 
+import warnings
+
 # Useful aliases for commonly used objects and modules.
 # Allows from pymatgen import <class> for quick usage.
 # Note that these have to come after the SETTINGS have been loaded. Otherwise, import does not work.
@@ -27,3 +29,13 @@ from .core.structure import IMolecule, IStructure, Molecule, Structure  # noqa
 from .core.units import ArrayWithUnit, FloatWithUnit, Unit  # noqa
 from .electronic_structure.core import Orbital, Spin  # noqa
 from .ext.matproj import MPRester  # noqa
+
+
+warnings.warn(
+    "All convenience imports in pymatgen/__init__.py will be removed from pymatgen>=2022.0.3. Please see "
+    "https://pymatgen.org/compatibility.html for instructions on how to make your code compatible with newer versions "
+    "of pymatgen. You should no longer use from pymatgen import Element, but rather from pymatgen.core import Structre."
+    " MPRester should be imported from pymatgen.ext.matproj. This will still be compatible with older versions of "
+    "pymatgen",
+    FutureWarning,
+)
